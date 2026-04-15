dbc03b2bd5cc95e076f5074347c41461ac0558f9
miu sugihara <hsha24055@ict.shimanet.ed.jp>
2026-04-15 10:42:41 +0900

    初回コミット

---
+++ script.js
@@ -0,0 +1,19 @@
+//おみくじの結果を配列で用意
+const fortunes = [
+    "大吉　💕　素敵な出会いがあるかも！",
+    "中吉　(^_-)-☆　少し勇気を出すとよいことが起きる",
+    "小吉　^^　今日はゆっくり過ごそう",
+    "凶　😭　無理せず慎重に行動しよう",
+];
+//ボタンと表示エリアを取得
+const button = document.getElementById("omikuji-bt");
+const result = document.getElementById("result");
+
+//ボタンがクリックされたときの処理
+button.addEventListener("click",function () {
+    //ランダムな数字を作る
+    const randomNumber = Math.floor(Math.random() * fortunes.length);
+
+    //結果を表示
+    result.textContent = fortunes[(randomNumber)];
+})
